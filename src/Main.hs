dc2b7421674a0b401d01af4a487a0702f4798102
Kaitian Xie <xkaitian@gmail.com>
2017-11-24 00:22:14 -0800

    Add sudoku io

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,11 @@
 module Main where
 
+import Data.Matrix
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  putStrLn "Enter an impcomplete sudoku: "
+  input <- getLine
+  let arr = read input :: [[Integer]]
+  let m = fromLists arr
+  print m
