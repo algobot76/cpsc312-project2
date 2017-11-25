9db016a0a4b2d99b79865ef246695f753c7d7b76
Kaitian Xie <xkaitian@gmail.com>
2017-11-24 17:33:05 -0800

    Implement isUnique and isValid

--- src/Main.hs
+++ src/Main.hs
@@ -53,3 +53,26 @@ containsZero [] = False
 containsZero (r:rs)
   | 0 `elem` r = True
   | otherwise = containsZero rs
+
+-- Check if a list has unique elements
+isUnique :: [Integer] -> Bool
+isUnique [] = True
+isUnique (x:xs) = x `notElem` xs && isUnique xs
+
+-- Check if a sudoku is valid
+isValid :: [[Integer]] -> Bool
+isValid sudoku =
+  containsZero sudoku &&
+  checkRows sudoku 0 && checkCols sudoku 0 && checkDiagonals sudoku
+  where
+    checkRows :: [[Integer]] -> Int -> Bool
+    checkRows _ 9 = True
+    checkRows _ n =
+      isUnique (getRow sudoku n) && isUnique (getRow sudoku (n + 1))
+    checkCols :: [[Integer]] -> Int -> Bool
+    checkCols _ 9 = True
+    checkCols _ n =
+      isUnique (getCol sudoku n) && isUnique (getCol sudoku (n + 1))
+    checkDiagonals :: [[Integer]] -> Bool
+    checkDiagonals _ =
+      isUnique (getMainDiagonal sudoku) && isUnique (getAntidiagonal sudoku)
